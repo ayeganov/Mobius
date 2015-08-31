9ac57849ab5269fabf55826382db300a4f7b77ff
Aleksandr Yeganov <absolutnik@gmail.com>
2015-08-31 08:34:05 -0400

    Refactor comm module
    1) Delete yaml config.
    2) Create python config.
    3) Add UploadFile message.
    4) Add generated python messages based msg.proto

--- mobius/comm/comm_config.py
+++ mobius/comm/comm_config.py
@@ -1,26 +1,85 @@
-import os
-
-import yaml
-
-from mobius.comm import stream
-
-CHANNEL_MAP = os.path.join(os.path.dirname(__file__), "channel_map.yaml")
+from mobius.comm import msg_pb2
+from mobius.utils import Singleton
 
 
-def create_stream_info(chan_name, msg_types):
-    stream_info = stream.StreamInfo(chan_name, **msg_types)
-    return stream_info
+STREAM_MAP =\
+    {
+        "/upload/ready/": dict(
+            send_type=msg_pb2.UploadFile
+            ),
+    }
 
 
-def create_stream_map():
-    try:
-        with open(CHANNEL_MAP, "r") as f:
-            file_contents = f.read()
+class StreamInfo:
+    '''
+    This class contains all of the necessary information to create a stream.
+    '''
+    def __init__(self, name, send_type, recv_type=None):
+        '''
+        Initializes the instance of StreamInfo.
 
-        channels = yaml.load(file_contents)
+        @param name - name of this stream
+        @param send_type - type of message to be sent over this stream
+        @param recv_type - type of message to receive in response.
+        '''
+        self._name = name
+        self._send_type = send_type
+        if recv_type is None:
+            self._recv_type = send_type
+        else:
+            self._recv_type = recv_type
 
-        streams = {chan: create_stream_info(chan, msg) for chan, msg in channels.items()}
-        return streams
-    except Exception as e:
-        print(e)
-        return None
+    @property
+    def name(self):
+        '''
+        Return name of this stream
+        '''
+        return self._name
+
+    @property
+    def send_type(self):
+        '''
+        Message type that can be sent over this stream.
+        '''
+        return self._send_type
+
+    @property
+    def recv_type(self):
+        '''
+        Message type that can be received in response over this stream.
+        '''
+        return self._recv_type
+
+
+class StreamMap(metaclass=Singleton):
+    '''
+    This class contains all information about the available channels.
+    '''
+    def __init__(self):
+        '''
+        Initializes the instance of StreamMap.
+        '''
+        self._stream_infos = {name: self._create_stream_info(name, msg)
+                              for name, msg in STREAM_MAP.items()}
+
+    def _create_stream_info(self, chan_name, msg_types):
+        '''
+        Helper method to turn a config entry into a stream info object.
+
+        @param chan_name - name of the channel
+        @param msg_types - types of messages associated with this channel
+        '''
+        stream_info = StreamInfo(chan_name, **msg_types)
+        return stream_info
+
+    def get_stream_name(self, chan_name):
+        '''
+        Look up the stream info associated with the given channel name
+
+        @param chan_name - name of the channel
+        @return StreamInfo associated with the given channel name, or None
+        '''
+        try:
+            return self._stream_infos[chan_name]
+        except KeyError:
+            raise stream.StreamError("Channel '{0}' doesn't exist.".format(chan_name))
