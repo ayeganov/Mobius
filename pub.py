a0fe33383990d8fd6f3a24c10f435e17acefb230
Aleksandr Yeganov <absolutnik@gmail.com>
2015-09-06 14:00:24 -0400

    Publisher test program refactor

--- pub.py
+++ pub.py
@@ -1,3 +1,4 @@
+import concurrent.futures
 import time
 
 from tornado import ioloop
@@ -14,14 +15,33 @@ loop = ioloop.IOLoop.instance()
 def msg_sent(msg):
     print("Message sent: {0}".format(msg[0].path))
 
-pub = SocketFactory.pub_socket("/upload/ready/", on_send=msg_sent, loop=loop)
-print("Address of zmq socket: {0}".format(pub._path))
 
-time.sleep(0.5)
-up_file = UploadFile(path="/run/shm/test.bar")
-pub.send(up_file)
+def send_file():
+    pub_sock = SocketFactory.pub_socket("/upload/ready/", on_send=msg_sent, loop=loop)
+    print("Address of zmq socket: {0}".format(pub_sock._path))
+
+    time.sleep(0.5)
+    the_file = UploadFile(path="/run/shm/test.bar")
+    pub_sock.send(the_file)
+
+
+def done(future):
+    error = future.exception()
+    if error is not None:
+        print("Oops, error: {0}".format(error))
+        return
+
+    print("Message sent successfully.")
+
+
+def start_up(executor):
+    result = executor.submit(send_file)
+    result.add_done_callback(done)
 
 try:
-    loop.start()
+    with concurrent.futures.ProcessPoolExecutor() as executor:
+        loop.add_callback(lambda: start_up(executor))
+        loop.start()
+
 except (KeyboardInterrupt, SystemExit):
     print("Exiting due to system interrupt...")
