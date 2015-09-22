1f4806718417121e5e6212af81973c6d3bf8b405
Aleksandr Yeganov <ayeganov@zoll.com>
2015-09-22 16:46:27 -0400

    IPC request proxy.
    This proxy makes sure the results get sent back to the appropriate
    tornado server that issued the http request.

--- mobius/comm/request_proxy.py
+++ mobius/comm/request_proxy.py
@@ -1,39 +1,26 @@
 import logging
 
-from mobius.comm.stream import SocketFactory
+from zmq.eventloop import IOLoop
 
+from mobius.comm import stream
+from mobius.utils import set_up_logging
 
 log = logging.getLogger(__name__)
 
-class RequestProxy:
-    '''
-    This class is responsible for routing traffic from the tornado servers to
-    service workers.
-    '''
-    def __init__(self, loop):
-        '''
-        Initializes an instance of RequestProxy.
 
-        @param loop - loop to be used by this instance
-        '''
-        self._loop = loop
-        self._server_in = SocketFactory.router_socket(
-                          "/request/request",
-                          on_recv=self._route_request,
-                          loop=loop)
+def main():
+    try:
+        set_up_logging()
+        loop = IOLoop.instance()
+        proxy = stream.RouterPubSubProxy(front="/request/request",
+                                         back_out="/request/do_work",
+                                         back_in="/request/result",
+                                         loop=loop)
+        log.info("Starting the server proxy.")
+        proxy.start()
+    except (SystemExit, KeyboardInterrupt):
+        print("Exiting due to system interrupt...")
 
-        self._server_out = SocketFactory.pub_socket(
-                          "/request/do_work",
-                          loop=loop)
 
-        self._service_in = SocketFactory.router_socket(
-                           "/request/result",
-                           on_recv=self._route_result,
-                           loop=loop)
-
-    def _route_request(self, msgs):
-        '''
-        Route the request to all of the service workers.
-
-        @param msgs - a list requst messages.
-        '''
+if __name__ == "__main__":
+    main()
