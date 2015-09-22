05cb801ad16023847d9ff40c1cff92e547c1b09c
Aleksandr Yeganov <ayeganov@zoll.com>
2015-09-22 16:54:38 -0400

    Add test request handler to server
    This handler is temporary, will be removed quite soon.

--- mobius/www/server.py
+++ mobius/www/server.py
@@ -1,17 +1,27 @@
 # Standard lib
+import logging
 import os
 import traceback
 
 
 # 3rd party
-from tornado import ioloop
+from zmq import eventloop
+from tornado.concurrent import Future
+from tornado import gen
 from tornado.httpserver import HTTPServer
 from tornado.web import (RequestHandler,
                          Application,
                          StaticFileHandler)
 
-from mobius.www.handlers import upload
+from mobius.comm import stream
+from mobius.comm.msg_pb2 import Request, MobiusModel
 from mobius.comm.stream import SocketFactory
+from mobius.service import QUOTE, UPLOAD, TEST
+from mobius.www.handlers import upload
+from mobius.utils import set_up_logging
+
+
+log = logging.getLogger(__name__)
 
 # TODO: Fix this by creating a util module
 TMP_DIR = "/run/shm/"
@@ -27,12 +37,61 @@ class MainHandler(RequestHandler):
         self.render("index.html")
 
 
+class TestHandler(RequestHandler):
+    '''
+    Remove me.
+    '''
+    def initialize(self, loop):
+        self._loop = loop
+        self._request_dealer = SocketFactory.dealer_socket("/request/local",
+                                                           on_recv=self._process_result,
+                                                           transport=stream.INPROC,
+                                                           bind=False,
+                                                           loop=loop)
+
+        self._request_future = None
+
+    def _process_result(self, msgs):
+        response = msgs[-1]
+        log.info("Response: {0}".format(msgs))
+        self._request_future.set_result(response)
+
+    @gen.coroutine
+    def get(self):
+        log.info("Test handler get")
+        self._request_future = Future()
+
+        mob_model = MobiusModel(id="hello", user_id="world")
+        request = Request(type=UPLOAD,
+                          model=mob_model)
+        self._request_dealer.send(request)
+
+        log.info("Lets wait here one second.")
+        yield self._request_future
+        log.info("One second should have passed.")
+
+        response = self._request_future.result()
+        print("Type of response: {0}".format(type(response)))
+        if response.HasField("error"):
+            self.set_status(500)
+            self.write(response.error)
+        elif response.HasField("response"):
+            self.set_status(200)
+            self.write(response.response)
+        self._request_future = None
+
+
 def main():
     '''
     Main routine, what more do you want?
     '''
     try:
+        set_up_logging()
+        loop = eventloop.IOLoop.instance()
         upload_pub = SocketFactory.pub_socket("/upload/ready/")
+        local_proxy = stream.LocalRequestProxy(front_end_name="/request/local",
+                                               back_end_name="/request/request",
+                                               loop=loop)
         app = Application(
             [
                 # Static file handlers
@@ -40,6 +99,8 @@ def main():
 
                 # File upload handler
                 (r'/upload', upload.StreamHandler, {"tmp_dir": TMP_DIR, "upload_pub": upload_pub}),
+
+                (r'/test', TestHandler, {"loop": loop}),
 
                 # Page handlers
                 (r"/", MainHandler),
@@ -52,7 +113,6 @@ def main():
         server = HTTPServer(app, max_body_size=MAX_BUFFER)
 
         server.listen(8888)
-        loop = ioloop.IOLoop.instance()
         print("Started mobius server.")
         loop.start()
     except (SystemExit, KeyboardInterrupt):
